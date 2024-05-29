9d6c16e0961177c44b4ceb714b5e08908ffaefdf
ellaiv <iovumihaela4@gmail.com>
2024-05-29 16:36:11 +0300

    Fixing the navigation bar and improving the visual appearance.

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,9 @@ const Home = () => {
         <img src={siteLogo2} alt="Site Logo2" className="image2" />
         <img src={siteLogo3} alt="Site Logo3" className="image3" />
       </div>
-      <section class="banner">
+      <section className="banner">
         <h2>Welcome to Little Lemon Restaurant!</h2>
-        <div class="schedule">
+        <div className="schedule">
           <div>Monday-Friday: 09:00 AM - 10:00 AM</div>
           <div>Saturday: 09:00 AM - 12:00 AM</div>
           <div>Sunday: 09:00 AM - 12:00 AM</div>
@@ -27,10 +27,12 @@ const Home = () => {
             src="https://www.fromvalerieskitchen.com/wordpress/wp-content/uploads/2023/06/Sirloin-Steak-24.jpg"
             alt="Steak"
           />
-          <h2 className="text">Best Steak Marinade in Existence</h2>
-          <p className="p">
-            You'll be shocked at the difference a good marinade makes!
-          </p>
+          <div className="description">
+            <h2 className="text">Baked marinated steak</h2>
+            <p className="p">
+              You'll be shocked at the difference a good marinade makes!
+            </p>
+          </div>
         </article>
         <article className="second">
           <img
@@ -38,12 +40,13 @@ const Home = () => {
             src="https://www.iheartnaptime.net/wp-content/uploads/2023/02/Beef-Tenderloin-I-Heart-Naptime-23.jpg"
             alt="Beef"
           />
-          <h2 className="text">Beef with lemon juice</h2>
-          <p className="p">
-            Perfectly tender and juicy Greek lemon beef stew simmered in a
-            delicious lemony flavoured sauce to form an amazing full of
-            Mediterranean flavours hearty dish
-          </p>
+          <div className="description">
+            <h2 className="text">Beef with lemon juice</h2>
+            <p className="p">
+              Explore the juicy delight of lemon-infused beef stew simmered to
+              perfection.
+            </p>
+          </div>
         </article>
         <article className="third">
           <img
@@ -51,11 +54,13 @@ const Home = () => {
             src="https://www.cocktailcontessa.com/wp-content/uploads/2022/06/bourbon-lemonade-14-768x1024.jpg"
             alt="Lemonade"
           />
-          <h2 className="text">Our best lemonade</h2>
-          <p className="p">
-            Our easy method for perfect lemonade combines simple syrup and fresh
-            lemon juice for a tart, sweet, and refreshing glass.
-          </p>
+          <div className="description">
+            <h2 className="text">Our best lemonade</h2>
+            <p className="p">
+              Enjoy our top-tier lemonade with fresh ingredients and a perfect
+              sweet-tart balance.
+            </p>
+          </div>
         </article>
       </section>
     </main>
